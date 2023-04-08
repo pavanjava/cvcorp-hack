b063970dadab1e8deecef1417c7a1b3fc0d31cbb
Pavan Mantha <manthapavankumar11@gmail.com>
2023-04-08 18:48:14 +0530

    initial commit of apis

---
+++ apis/src/test/java/com/sparkdev/ofd/apis/ApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkdev.ofd.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
